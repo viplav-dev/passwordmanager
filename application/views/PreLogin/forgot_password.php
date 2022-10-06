66e7b139e1b57971bcd756ad1c5910fb30846e68
VIplav Pandhurnekar <pandhurnekarviplav@gmail.com>
2022-10-06 16:50:51 +0530

    Added back button to forgot password

--- application/views/PreLogin/forgot_password.php
+++ application/views/PreLogin/forgot_password.php
@@ -2,6 +2,9 @@
     <div class="signInContainer">
 
         <div class="signInForm">
+            <a href="<?php echo base_url('account/signin'); ?>"><i class="fa-solid fa-arrow-left"></i> Return to Login</a>
+            <br>
+            <br>
             <div class="signInTitle">Forgot Password</div>
             <p style="max-width:325px;">Please enter your registered email address to receive password reset link.</p>
             <?php
@@ -15,9 +18,9 @@
             <label for="email">Email:</label>
             <input class="signInInput" placeholder="tony@starkindustries.com" name="email" type="email" autofocus required>
 
-            
+
             <div class="signInBtnContainer" style="justify-content:end!important;">
-           
+
                 <input type="submit" style="width:fit-content!important ;" class="signInBtn" class="btn btn-lg btn-success btn-block" value="Send Link" />
             </div>
             </form>
