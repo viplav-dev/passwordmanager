6392e60736f7acf6e783e95e1a276c83926b847a
VIplav Pandhurnekar <pandhurnekarviplav@gmail.com>
2022-10-08 20:00:16 +0530

    auto deploy

---
+++ application/controllers/Deploy.php
@@ -0,0 +1,60 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class Deploy extends CI_Controller
+{
+
+
+    function __construct()
+    {
+        parent::__construct();;
+    }
+
+    public function index()
+    {
+        /**
+         * GIT DEPLOYMENT SCRIPT
+         *
+         * Used for automatically deploying websites via GitHub
+         *
+         */
+
+        // array of commands
+        $commands = array(
+            'echo $PWD',
+            'whoami',
+            'git pull',
+            'git status',
+            'git submodule sync',
+            'git submodule update',
+            'git submodule status',
+        );
+
+        // exec commands
+        $output = '';
+        foreach ($commands as $command) {
+            $tmp = shell_exec($command);
+
+            $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
+            $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+        }
+        echo '
+
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8">
+    <title>GIT DEPLOYMENT SCRIPT</title>
+</head>
+<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
+<div style="width:700px">
+    <div style="float:left;width:350px;">
+    <p style="color:white;">Git Deployment Script</p>
+    <?php echo $output; ?>
+    </div>
+</div>
+</body>
+</html>
+		';
+    }
+}
