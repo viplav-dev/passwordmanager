9d2ef2e8ae34b3cc556c7b90f3c06c68e39ba6d1
VIplav Pandhurnekar <pandhurnekarviplav@gmail.com>
2022-10-09 01:59:49 +0530

    d

--- application/controllers/Deploy.php
+++ application/controllers/Deploy.php
@@ -23,11 +23,11 @@ class Deploy extends CI_Controller
         $commands = array(
             'cd',
             'bash autogit.sh',
-            'cd /var/www/passwordmanager git pull',
-            'cd /var/www/passwordmanager git status',
-            'cd /var/www/passwordmanager git submodule sync',
-            'cd /var/www/passwordmanager git submodule update',
-            'cd /var/www/passwordmanager git submodule status',
+            'git pull',
+            'git status',
+            'git submodule sync',
+            'git submodule update',
+            'git submodule status',
         );
 
         // exec commands
